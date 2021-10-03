fc16ccb6274bc6362d3edf694244667b6718ab69
YiZhenBang <1628755314@qq.com>
2021-10-03 14:56:32 +0800

    Druid 配置文件版本

---
+++ mycroboot-database/src/main/java/com/yzb/database/StarterDruidApplication.java
@@ -0,0 +1,20 @@
+package com.yzb.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: StaeterDruidApplication
+ * Description:
+ * date: 2021/10/3 14:45
+ *
+ * @author ZhenBang-Yi
+ * @version 1.0
+ * @since JDK 1.8
+ */
+@SpringBootApplication
+public class StarterDruidApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StarterDruidApplication.class, args);
+    }
+}
---
+++ mycroboot-database/src/test/java/com/yzb/TestDruidDataSource.java
@@ -0,0 +1,37 @@
+package com.yzb;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.yzb.database.StarterDruidApplication;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ * ClassName: TestDruidDatabSource
+ * Description:
+ * date: 2021/10/3 14:44
+ *
+ * @author ZhenBang-Yi
+ * @version 1.0
+ * @since JDK 1.8
+ */
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration
+@SpringBootTest(classes = StarterDruidApplication.class)
+public class TestDruidDataSource {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    void getConn() throws SQLException {
+        System.out.println(dataSource.getConnection());
+        System.out.println(dataSource);
+    }
+}
