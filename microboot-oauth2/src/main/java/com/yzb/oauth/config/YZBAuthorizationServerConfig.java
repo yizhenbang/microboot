d47bb6e89c3de385cb040d6d72e8b7376dc24179
YiZhenBang <1628755314@qq.com>
2021-10-15 23:14:22 +0800

    将数Token据保存在redis中

--- microboot-oauth2/src/main/java/com/yzb/oauth/config/YZBAuthorizationServerConfig.java
+++ microboot-oauth2/src/main/java/com/yzb/oauth/config/YZBAuthorizationServerConfig.java
@@ -3,18 +3,42 @@ package com.yzb.oauth.config;
 import com.yzb.oauth.service.ClientDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class YZBAuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Autowired
     private ClientDetailsServiceImpl clientDetailsService;
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.withClientDetails(clientDetailsService);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(new RedisTokenStore(this.redisConnectionFactory)); // 要通过Redis保存Token
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.allowFormAuthenticationForClients() // 允许通过Form表单来实现客户端认证
+                .checkTokenAccess("isAuthenticated()") // 认证之后才可以实现Token查询
+                .tokenKeyAccess("permitAll()") // 获取Token的时候不进行拦截
+                .passwordEncoder(this.passwordEncoder); // 客户端登录的时候是需要进行密码输入的
     }
 }
--- microboot-oauth2/src/main/java/com/yzb/oauth/service/ClientDetailsServiceImpl.java
+++ microboot-oauth2/src/main/java/com/yzb/oauth/service/ClientDetailsServiceImpl.java
@@ -1,5 +1,7 @@
 package com.yzb.oauth.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.ClientRegistrationException;
@@ -12,17 +14,21 @@ import java.util.Set;
 
 @Service
 public class ClientDetailsServiceImpl implements ClientDetailsService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public ClientDetails loadClientByClientId(String clientId) throws ClientRegistrationException {
         BaseClientDetails client = new BaseClientDetails();
         client.setClientId("client_yzb");//设置Client_id
-        client.setClientSecret("hello");
+        client.setClientSecret(this.passwordEncoder.encode("hello")); //不进行加密处理会报错
         client.setAuthorizedGrantTypes(Arrays.asList("authorization_code"));
         client.setScope(Arrays.asList("webapp"));
         client.isAutoApprove("true");//自动确认
-        client.setAccessTokenValiditySeconds(120);//Token有效时间为120秒
+        client.setAccessTokenValiditySeconds(150);//Token有效时间为120秒
         Set<String> hashSet = new HashSet();
-        hashSet.add("https://yootk.com");
+        hashSet.add("https://www.yootk.com");
         client.setRegisteredRedirectUri(hashSet);//要是不设置这个就会导致没有办法获取到   authCode
         return client;
     }
